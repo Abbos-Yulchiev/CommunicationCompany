ac928914a2f066ac8062f529e2035295824b1b69
Abbos <yulchiyevabbos1@gamil.com>
2021-04-23 11:12:16 +0500

    creating repositories

---
+++ src/main/java/uz/pdp/communicationcompany/CommunicationCompanyApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.communicationcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicationCompanyApplication.class, args);
+    }
+
+}
